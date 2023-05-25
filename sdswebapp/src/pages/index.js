bb0e3c4f5bde695c0db66bd6bdbeef4bb3346400
AaronHernandezPortfolio <103552269+AaronHernandezPortfolio@users.noreply.github.com>
2023-05-25 10:57:56 -0700

    fixed icon

--- sdswebapp/src/pages/index.js
+++ sdswebapp/src/pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/SDS.svg" />
       </Head>
       <main>
       <Navbar />
